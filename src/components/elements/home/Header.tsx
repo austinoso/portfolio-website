8b3067ed7149a78c60ab0fd2dad128914c6ad49e
austinoso <contact@austinosor.io>
2023-01-09 06:40:21 +0000

    replace linkicon with codeicon replace resume link with linkedin

--- src/components/elements/home/Header.tsx
+++ src/components/elements/home/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DocumentArrowDownIcon, LinkIcon } from "@heroicons/react/20/solid";
+import { UserIcon, CodeBracketIcon } from "@heroicons/react/20/solid";
 
 function Header() {
   return (
@@ -24,16 +24,19 @@ function Header() {
             JavaScript frameworks.
           </p>
           <div className="flex gap-2">
-            <button
-              type="button"
-              className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            <a
+              href="https://www.linkedin.com/in/austinoso/"
+              target="_blank"
+              rel="noreferrer"
             >
-              My Resume
-              <DocumentArrowDownIcon
-                className="ml-1 -mr-1 h-5 w-5"
-                aria-hidden="true"
-              />
-            </button>
+              <button
+                type="button"
+                className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                LinkedIn
+                <UserIcon className="ml-1 -mr-1 h-5 w-5" aria-hidden="true" />
+              </button>
+            </a>
             <a
               href="https://github.com/austinoso"
               target="_blank"
@@ -44,7 +47,10 @@ function Header() {
                 className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
               >
                 GitHub
-                <LinkIcon className="ml-1 -mr-1 h-5 w-5" aria-hidden="true" />
+                <CodeBracketIcon
+                  className="ml-1 -mr-1 h-5 w-5"
+                  aria-hidden="true"
+                />
               </button>
             </a>
           </div>
