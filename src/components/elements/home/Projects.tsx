d9804e1b0a338f6995189bf6db8af41cf9d7acf6
austinoso <contact@austinosor.io>
2023-03-09 23:45:43 +0000

    add rel="noreferrer"

--- src/components/elements/home/Projects.tsx
+++ src/components/elements/home/Projects.tsx
@@ -47,6 +47,7 @@ export default function Projects(props: ProjectsProps) {
               className="flex flex-col overflow-hidden rounded-lg shadow-lg"
               href={project.attributes.github}
               target="_blank"
+              rel="noreferrer"
             >
               <div className="flex flex-1 flex-col justify-between bg-white p-6">
                 <div className="flex-1">
