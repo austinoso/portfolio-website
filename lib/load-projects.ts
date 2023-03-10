24b6244226b3cfabe582b2b24c340a5822e81be7
austinoso <contact@austinosor.io>
2023-03-10 12:15:31 +0000

    add loadProjects list   * for now, get only 10 projects and sort on relevance

--- lib/load-projects.ts
+++ lib/load-projects.ts
@@ -6,3 +6,12 @@ export async function loadProjects() {
 
   return projects;
 }
+
+export async function loadProjectsList() {
+  const res = await fetch(
+    "http://51.81.211.148:1337/api/projects?populate=%2A&pagination[page]=1&pagination[pageSize]=10&sort=relevance%3Adesc"
+  );
+  const projects = await res.json();
+
+  return projects;
+}
