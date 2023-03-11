d425c04fbb653fc7f6d0cafa20c75b2fb26213d0
austinoso <contact@austinosor.io>
2023-03-11 11:14:35 +0000

    remove console.logs

--- src/components/elements/projects/ProjectDetails.tsx
+++ src/components/elements/projects/ProjectDetails.tsx
@@ -4,7 +4,6 @@ interface ProjectDetailsProps {
 
 export default function ProjectDetails(props: ProjectDetailsProps) {
   const project = props.project.project.data;
-  console.log(project);
 
   return (
     <div className="relative px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-12 lg:pb-28">
