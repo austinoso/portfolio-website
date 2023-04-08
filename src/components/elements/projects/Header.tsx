e7acafce97d9e6db659b9dafd259f391a864b549
austinoso <contact@austinosor.io>
2023-04-08 10:09:20 +0000

    new header

--- src/components/elements/projects/Header.tsx
+++ src/components/elements/projects/Header.tsx
@@ -1,48 +1,86 @@
-import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-function Header() {
+const navigation = [{ name: "Home", href: "/" }];
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
-    <div
-      className="
-        text-white bg-gradient-to-br from-teal-500 to-blue-500
-          py-24
-          text-center
-          relative
-      "
-    >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold">All Projects</h1>
-        </div>
-      </div>
-      {/* <div className="mt-12 sm:mx-auto sm:flex sm:max-w-3xl absolute -bottom-6 left-1/2 transform -translate-x-1/2">
-        <label htmlFor="search" className="sr-only">
-          Search projects
-        </label>
-        <div className="mt-1 flex rounded-md shadow-sm">
-          <div className="relative flex flex-grow items-stretch focus-within:z-10">
-            <input
-              type="search"
-              name="search"
-              id="search"
-              className="block w-full rounded-none rounded-l-md border-gray-300 p-2 focus:border-blue-500 focus:ring-blue-500 sm:text-lg text-gray-900"
-              placeholder="Search projects"
-            />
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex items-center gap-x-12">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Austin Osorio</span>
+            <img className="h-7 w-auto" src="/images/logo.svg" alt="" />
+          </Link>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name}
+              </Link>
+            ))}
           </div>
+        </div>
+        <div className="flex lg:hidden">
           <button
             type="button"
-            className="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
           >
-            <MagnifyingGlassIcon
-              className="h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-      </div> */}
-    </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img className="h-8 w-auto" src="/images/logo.svg" alt="" />
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
   );
 }
-
-export default Header;
