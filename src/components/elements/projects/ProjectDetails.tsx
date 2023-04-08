5acda930bbf7139601dd72ed4f189516ff7b188a
austinoso <contact@austinosor.io>
2023-04-08 10:14:37 +0000

    display project title

--- src/components/elements/projects/ProjectDetails.tsx
+++ src/components/elements/projects/ProjectDetails.tsx
@@ -14,10 +14,11 @@ export default function ProjectDetails(props: ProjectDetailsProps) {
     <div className="relative px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-12 lg:pb-28">
       <div className="relative mx-auto max-w-7xl flex flex-row flex-wrap">
         <div className="w-full sm:w-1/3 md:w-1/4 p-4">
-          <h3 className="text-blue-500 text-lg font-bold">Other Projects</h3>
+          <h3 className="text-blue-600 text-lg font-bold">Other Projects</h3>
           <ProjectNav relivantProjects={relivantProjects} project={project} />
         </div>
         <div className="w-full sm:w-2/3 md:w-3/4 p-4 flex flex-col space-y-4">
+          <h2 className="text-xl font-bold">{project.attributes.Title}</h2>
           <ReactMarkdown>{project.attributes.Description}</ReactMarkdown>
           <div>
             <a
