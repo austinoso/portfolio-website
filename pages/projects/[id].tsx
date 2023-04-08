bbfa99191611bad5b7fd31e0a531f95a5f2ac77b
austinoso <contact@austinosor.io>
2023-04-08 10:12:59 +0000

    add new header

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getProject, loadProjectsList } from "../../lib/load-projects";
 import ProjectDetails from "../../src/components/elements/projects/ProjectDetails";
+import Header from "../../src/components/elements/projects/Header";
 
 function Project(projectData: any) {
   const project = projectData.project.data;
@@ -8,21 +9,7 @@ function Project(projectData: any) {
 
   return (
     <>
-      <div
-        className="
-        text-white bg-gradient-to-br from-teal-500 to-blue-500
-          py-24
-          text-center
-          relative
-      "
-      >
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-          <div className="space-y-2">
-            <h1 className="text-3xl font-bold">{project.attributes.Title}</h1>
-            <p className="text-lg">{project.attributes.Type}</p>
-          </div>
-        </div>
-      </div>
+      <Header />
       <ProjectDetails
         project={projectData}
         relivantProjects={relivantProjects}
