b5720ea8dda14477ef1b839e73388b77c82cbcfd
austinoso <contact@austinosor.io>
2023-04-09 10:30:42 +0000

    removed unwanted styles

--- src/layouts/ProjectLayout.tsx
+++ src/layouts/ProjectLayout.tsx
@@ -4,10 +4,8 @@ import { ProjectProvider } from "../contexts/project.context";
 export default function ProjectLayout({ children }: any) {
   return (
     <ProjectProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        {children}
-      </div>
+      <Header />
+      {children}
     </ProjectProvider>
   );
 }
