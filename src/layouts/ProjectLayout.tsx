d059c5ee6498c646ca91f93817acb528166079ee
austinoso <contact@austinosor.io>
2023-04-10 09:44:43 +0000

    add ProjectBreadCrumbs to ProjectLayout

--- src/layouts/ProjectLayout.tsx
+++ src/layouts/ProjectLayout.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/elements/projects/Header";
 import { ProjectProvider } from "../contexts/project.context";
+import ProjectBreadCrumbs from "../components/elements/projects/ProjectBreadCrumbs";
 
 export default function ProjectLayout({ children, project, projects }: any) {
   return (
     <ProjectProvider currentProject={project} projects={projects}>
       <Header />
+      <ProjectBreadCrumbs />
       {children}
     </ProjectProvider>
   );
