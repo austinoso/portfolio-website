f46ff7eecf9246dc29725df9fe861e72a65f1599
austinoso <contact@austinosor.io>
2023-04-11 10:45:39 +0000

    project index meta tags

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,10 +3,29 @@ import type { ReactElement } from "react";
 import ProjectList from "../../src/components/elements/projects/ProjectList";
 import { loadProjectsList } from "../../lib/load-projects";
 import type { NextPageWithLayout } from "../../pages/_app";
+import Head from "next/head";
 
 const Projects: NextPageWithLayout = () => {
   return (
     <div>
+      <Head>
+        <title>Austin Osorio | Projects</title>
+        <meta name="title" content="Austin Osorio | Projects"></meta>
+        <meta
+          name="description"
+          content="Explore my software development portfolio showcasing proficiency in TypeScript, Next.js, Express, and NoSQL/SQL databases. Discover my capabilities as a developer."
+        ></meta>
+        <meta
+          name="keywords"
+          content="software engineer, Austin Osorio, California"
+        ></meta>
+        <meta name="robots" content="index, follow"></meta>
+        <meta
+          httpEquiv="Content-Type"
+          content="text/html; charset=utf-8"
+        ></meta>
+        <meta name="language" content="English"></meta>
+      </Head>
       <ProjectList />
     </div>
   );
